3a9263e316b138bc7af7ea9b031b8d696e6ed61c
LiuzF-05 <luizfsilvacs@gmail.com>
2025-06-18 19:40:04 -0300

    Minor alterations on the get_files_info and created the get_files_contents function which returns a string of the text content of a file and truncates it at 10000 characters

---
+++ functions/get_file_contents.py
@@ -0,0 +1,24 @@
+import os
+
+def get_file_contents(working_directory, file_path):
+    
+    #Creates the absolute file_path
+    abs_file_path=os.path.abspath(os.path.join(working_directory, file_path))
+
+    #Checks if the directory is in the pre defined working_directory
+    if not abs_file_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory\n'
+    
+    #Checks if the file_path leads to a file    
+    elif not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    #Catch exceptions and return the file's contents truncated at 10000 chars
+    try:
+        with open(abs_file_path, "r") as f:
+            file_content=f.read(10000)
+            if len(file_content)==10000:
+                return f'{file_content}[...File "{file_path}" truncated at 10000 characters]'
+        return file_content
+    except Exception as e:
+        return f'Error reading files {e}'
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,21 +4,22 @@ def get_files_info(working_directory, directory=None):
     
     dir_path=os.path.abspath(working_directory)
 
+    #Checks if there is a directory specified and sets the dir_path to its path
     if directory:
         dir_path=os.path.abspath(os.path.join(working_directory, directory))
 
+    #Checks if the directory is in the pre defined working_directory
+    if not dir_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
+
     #Checks if the directory path leads to an actual directory
-    if not os.path.isdir(dir_path):
+    elif not os.path.isdir(dir_path):
         return f'Error: "{directory}" is not a directory\n'
         
-
-    #Checks if the directory is in the pre defined working_directory
-    elif not dir_path.startswith(os.path.abspath(working_directory)):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
-        
-    
     #Return string
     result=("")
+
+    #Catch exceptions and create the return string
     try:
         for content in os.listdir(dir_path):
             result+=f"- {content}: file_size={os.path.getsize(os.path.abspath(os.path.join(dir_path, content)))}, is_dir={os.path.isdir(os.path.abspath(os.path.join(dir_path, content)))}\n"
