3373e3d37d6a6e8ca6e06acfd179c046fb6c465f
LiuzF-05 <luizfsilvacs@gmail.com>
2025-06-22 21:48:21 -0300

    Moved the call_function function to a separate file as well as changing the placement of some variables in main.py to make the code slightly cleaner. Also added a loop for the agent to call functions and remember the results for the next call, only printing once no more functions are being called. Lastly, changed the system prompt to stop the AI from asking for clarifications, as the program doesn't remember the messages from previous python processes in the current scope.

--- main.py
+++ main.py
@@ -3,58 +3,12 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-from functions.write_file import write_file
-from functions.run_python_file import run_python_file
 from functions.schemas import *
+from call_function import call_function
 
-def call_function(function_call_part, verbose=False):
 
-    #Defines the dictionary with all possible functions
-    funct_dict={"get_files_info": get_files_info,
-                "get_file_content": get_file_content,
-                "write_file": write_file,
-                "run_python_file": run_python_file,}
-    
-    #Adds a check for verbose, changing how much information is returned
-    if verbose:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(f" - Calling function: {function_call_part.name}")
-    
-    if not function_call_part.name in funct_dict:
-        return types.Content(
-            role="tool",
-            parts=[
-            types.Part.from_function_response(
-                name=function_call_part.name,
-                response={"error": f"Unknown function: {function_call_part.name}"},
-        )
-    ],
-)
-
-    #Calling the actual function
-    result=funct_dict[function_call_part.name](**{**function_call_part.args, 'working_directory': './calculator',})
-
-    return types.Content(
-    role="tool",
-    parts=[
-        types.Part.from_function_response(
-            name=function_call_part.name,
-            response={"result": result },
-        )
-    ],
-)
-
-def main():
-    #Loading the API key
-    load_dotenv()
-    api_key=os.environ.get("GEMINI_API_KEY")
-    client=genai.Client(api_key=api_key)
-
-    #Defining system prompt to decide personality and behaviours
-    system_prompt = """
+#Defining system prompt to decide personality and behaviours
+system_prompt = """
 You are a helpful AI coding agent.
 
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
@@ -65,7 +19,27 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+When in doubt of what the user might be referring to, use the tools to list the files and read the contents in order to find the most sensible match, letting the user know that it might be wrong due to prompt vagueness.
+
+You should never ask the user for clarifications as that is outside of your scope. Simply look for the most logical match to what the user is asking.
 """
+
+#List of available functions
+available_functions=types.Tool(function_declarations=[
+    schema_get_files_info,
+    schema_get_file_content,
+    schema_write_file,
+    schema_run_python_file,
+    ])
+
+def main():
+    verbose=False
+
+    #Loading the API key
+    load_dotenv()
+    api_key=os.environ.get("GEMINI_API_KEY")
+    client=genai.Client(api_key=api_key)
 
     #Checking for user prompt
     if len(sys.argv)<2:
@@ -73,44 +47,44 @@ All paths you provide should be relative to the working directory. You do not ne
         sys.exit(1)
     prompt=sys.argv[1]
 
+    #Checking for verbose
+    if len(sys.argv)>2 and sys.argv[2]=="--verbose":
+        verbose=True
+        
     #List of previous messages
     messages=[types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    #List of available functions
-    available_functions=types.Tool(function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_write_file,
-        schema_run_python_file,
-        ])
+    #Function calling loop, limited to 20 calls to avoid infinite function loops
+    for i in range(20):
 
-    #Generating Gemini's response
-    response=client.models.generate_content(
+        #Generating the prompt's response with the context of the messages list
+        response=client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
         config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]),)
+        for candidate in response.candidates: #type:ignore
+            messages.append(candidate.content) #type: ignore
 
-    #Checking for verbose arg
-    verbose=False
-    if len(sys.argv)>2 and sys.argv[2]=="--verbose":
-        verbose=True
+        #If a function is called by the agent, it's result is added to the messages. In case of no output it will raise an error.
+        if response.function_calls:
+            for response_part in response.function_calls:
+                funct_result=call_function(response_part, verbose)
+                if "error" in funct_result.parts[0].function_response.response: #type: ignore
+                    raise Exception ("Error: function did not generate a response")
+                if verbose:
+                    print(f"-> {funct_result.parts[0].function_response.response}") #type: ignore
+                messages.append(funct_result)
+
+        #Once the agent has stopped making calls it will break the loop and print the final response
+        else:
+            print(response.text)
+            break
+
+    #Checking for verbose arg and printing the required debug information
+    if verbose:
         prompt_tokens=response.usage_metadata.prompt_token_count #type: ignore
         response_tokens=response.usage_metadata.candidates_token_count #type: ignore
         print(f"User prompt: {prompt}\nPrompt tokens: {prompt_tokens} \nResponse tokens: {response_tokens}\n")
-
-    #Default sys out
-    if response.function_calls:
-        for response_part in response.function_calls:
-            funct_result=call_function(response_part, verbose)
-            if "error" in funct_result.parts[0].function_response.response: #type: ignore
-                raise Exception ("Error: function did not generate a response")
-            if verbose:
-                print(f"-> {funct_result.parts[0].function_response.response}") #type: ignore
-        print(response.text)
-    
-
-    else:            
-        print(response.text)
 
 if __name__=="__main__":
     main()
